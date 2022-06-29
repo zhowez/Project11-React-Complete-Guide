533447e4eef467172072835b82279df0778e8180
Zach Howes <zhowes14@icloud.com>
2022-06-28 21:01:50 -0400

    added another redux state

--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,25 @@
 import { createStore } from "redux";
 
-const counterReducer = (state = { counter: 0 }, action) => {
+const initalState = { counter: 0, showCounter: true };
+
+const counterReducer = (state = initalState, action) => {
   if (action.type === "increment") {
-    return { counter: state.counter + 1 };
+    return { counter: state.counter + 1, showCounter: state.showCounter };
   }
 
   if (action.type === "increase") {
-    return { counter: state.counter + action.amount };
+    return {
+      counter: state.counter + action.amount,
+      showCounter: state.showCounter,
+    };
   }
 
   if (action.type === "decrement") {
-    return { counter: state.counter - 1 };
+    return { counter: state.counter - 1, showCounter: state.showCounter };
+  }
+
+  if (action.type === "toggle") {
+    return { counter: state.counter, showCounter: !state.showCounter };
   }
 
   return state;
